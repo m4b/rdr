29ea3b26c41991c4506155a7b981a1839b96b516
m4b <m4b.github.io@gmail.com>
2015-07-24 13:23:09 -0600

    removed todos in LibRdr

--- lib/LibRdr.ml
+++ lib/LibRdr.ml
@@ -1,7 +1,4 @@
-(* todo: setup proper elf, mach base files again *)
 module Elf = Elf
 module Mach = Mach
 module Goblin = Goblin
-(* todo, make these Input.Binary , etc. *)
 module Utils = RdrUtils
-
