3de9933f40424c580a95ad0b4e0402c6e8c87dc3
m4b <m4b.github.io@gmail.com>
2015-09-05 13:06:44 -0600

    added macro-generated load command file; updated oasis and removed non-generated getters and pattern match on cmd to use macro generated functions;

--- lib/mach/MachLoadCommand.ml
+++ lib/mach/MachLoadCommand.ml
@@ -6,6 +6,7 @@
 module Types = MachLoadCommandTypes
 
 open MachLoadCommandTypes
+open MachLoadCommandMacro
 
 type t = lc list
 
@@ -124,182 +125,15 @@ let get_segment_by_name segname segments =
   try Some (List.find (fun segment -> segment.segname = segname) segments)
   with Not_found -> None
 
-(* specific load command constructors *)
-let get_symtable binary offset =
-  let cmd,cmdsize,o = get_load_command_header binary offset in
-  let symoff,o = Binary.u32o binary o in
-  let nsyms,o = Binary.u32o binary o in
-  let stroff,o =  Binary.u32o binary o in
-  let strsize =  Binary.u32 binary o in
-  {cmd; cmdsize; symoff; nsyms; stroff; strsize;}
-
-let get_dysymtable binary offset =
-  let cmd,cmdsize,o = get_load_command_header binary offset in
-  let ilocalsym,o = Binary.u32o binary o in
-  let nlocalsym,o = Binary.u32o binary o in
-
-  let iextdefsym,o =  Binary.u32o binary o in
-  let nextdefsym,o =  Binary.u32o binary o in
-
-  let iundefsym,o = Binary.u32o binary o in
-  let nundefsym,o = Binary.u32o binary o in
-
-  let tocoff,o =  Binary.u32o binary o in
-  let ntoc,o =  Binary.u32o binary o in
-  let modtaboff,o = Binary.u32o binary o in
-  let nmodtab,o = Binary.u32o binary o in
-
-  let extrefsymoff,o =  Binary.u32o binary o in
-  let nextrefsyms,o =  Binary.u32o binary o in
-
-  let indirectsymoff,o =  Binary.u32o binary o in
-  let nindirectsyms,o =  Binary.u32o binary o in
-  let extreloff,o =  Binary.u32o binary o in
-  let nextrel,o =  Binary.u32o binary o in
-  let locreloff,o =  Binary.u32o binary o in
-  let nlocrel =  Binary.u32 binary o in
-  {
-    cmd; cmdsize;
-    ilocalsym; nlocalsym; iextdefsym; nextdefsym;
-    iundefsym; nundefsym; tocoff; ntoc; modtaboff; nmodtab;
-    extrefsymoff; nextrefsyms;
-    indirectsymoff; nindirectsyms; extreloff; 
-    nextrel; locreloff; nlocrel;
-  }
-
-let get_dyld_info binary offset =
-  let cmd,cmdsize,o = get_load_command_header binary offset in
-  let rebase_off,o = Binary.u32o binary o in
-  let rebase_size,o = Binary.u32o binary o in
-  let bind_off,o =  Binary.u32o binary o in
-  let bind_size,o =  Binary.u32o binary o in
-  let weak_bind_off,o =  Binary.u32o binary o in
-  let weak_bind_size,o =  Binary.u32o binary o in
-  let lazy_bind_off,o =  Binary.u32o binary o in
-  let lazy_bind_size,o =  Binary.u32o binary o in
-  let export_off,o =  Binary.u32o binary o in
-  let export_size =  Binary.u32 binary o in
-  {cmd; cmdsize;
-   rebase_off; rebase_size; bind_off; bind_size;
-   weak_bind_off; weak_bind_size; lazy_bind_off;
-   lazy_bind_size; export_off; export_size;}
-
-(* consider glomming all libs into single binary with 0...N load commands and library indexed at 1... *)
-let get_dylib_command binary offset =
-  let cmd,cmdsize,o = get_load_command_header binary offset in
-  let stroffset,o = Binary.u32o binary o in
-  let timestamp,o = Binary.u32o binary o in
-  let current_version,o =  Binary.u32o binary o in
-  let compatibility_version =  Binary.u32 binary o in
-  let str = Binary.string binary (stroffset+offset) in (* technically should use the lc_str_offset but need (lc_str_offset - sizeof_load_command) because offset from start of the load_command and we chopped off the first 8 bytes of the lc*)
-  let name = {offset; str} in
-  let dylib = {name; timestamp; current_version; compatibility_version;} in
-  {cmd; cmdsize; dylib}
-
-(* version for osx and ios *)
-let get_version binary offset =
-  let cmd,cmdsize,o = get_load_command_header binary offset in
-  let version,o = Binary.u32o binary o in
-  let sdk = Binary.u32 binary o in
-  {cmd; cmdsize; version; sdk;}
-
-let get_main binary offset =
-  let cmd,cmdsize,o = get_load_command_header binary offset in
-  let entryoff,o = Binary.u64o binary o in
-  let stacksize = Binary.u64 binary o in
-  {cmd; cmdsize; entryoff; stacksize;}
-
-let get_dylinker binary offset =
-  let cmd,cmdsize,o = get_load_command_header binary offset in
-  let stroffset,o = Binary.u32o binary o in
-  let str = Binary.string binary (offset+stroffset) in
-  let name = {offset; str} in
-  {cmd; cmdsize; name;}
-
-let get_section64 binary offset =
-  (*   Printf.printf "initial o: %d\n" o; *)
-  let sectname,o = Binary.stringo binary offset ~num_bytes:16 in
-  (*   Printf.printf "sectname: %s o: %d\n" sectname o; *)
-  let segname,o = Binary.stringo binary o ~num_bytes:16 in
-  (*   Printf.printf "segname: %s o: %d\n" segname o; *)
-  let addr,o = Binary.u64o binary o in
-  let size,o = Binary.u64o binary o in
-  let offset,o = Binary.u32o binary o in
-  let align,o = Binary.u32o binary o in
-  let reloff,o = Binary.u32o binary o in
-  let nreloc,o = Binary.u32o binary o in
-  let flags,o = Binary.u32o binary o in
-  let reserved1,o = Binary.u32o binary o in
-  let reserved2,o = Binary.u32o binary o in
-  let reserved3 = Binary.u32 binary o in
-  {sectname; segname; addr; size; 
-   offset; align; reloff; nreloc; flags; 
-   reserved1; reserved2; reserved3;}
-
-let get_sections64 binary nsects offset =
-  let rec loop count acc =
-    if (count >= nsects) then
-      List.rev acc
-    else
-      let section = get_section64 binary ((sizeof_section_64*count)+offset) in
-      (*       Printf.printf "sec %d\n" count; *)
-      loop (count+1) (section::acc)
-  in loop 0 []
-
-let get_segment64 binary offset =
-  let cmd,cmdsize,o = get_load_command_header binary offset in
-  let segname,o = Binary.stringo ~num_bytes:16 binary o in
-  let vmaddr,o = Binary.u64o binary o in
-  let vmsize,o =  Binary.u64o binary o in
-  let fileoff,o = Binary.u64o binary o in
-  let filesize,o =  Binary.u64o binary o in
-  let maxprot,o = Binary.u32o binary o in
-  let initprot,o = Binary.u32o binary o in
-  let nsects,o = Binary.u32o binary o in
-  let flags,o = Binary.u32o binary o in
-  (*   Printf.printf "vmaddr: 0x%x vmsize 0x%x fileoff 0x%x filesize 0x%x maxprot %d initprot %d nsects %d %d flags\n" vmaddr vmsize fileoff filesize maxprot initprot nsects flags; *)
-  let sections = get_sections64 binary nsects o in
-  {cmd; cmdsize; segname; vmaddr; vmsize;
-   fileoff; filesize; maxprot; initprot;
-   nsects; flags; sections;}
-
 let rec get_load_commands_it binary offset ncmds acc =
   if (ncmds <= 0) then
     List.rev acc
   else
     let cmdi,cmdsize,_ = get_load_command_header binary offset in
     let cmd = cmdi |> to_cmd in
-    (*     let bytes = Bytes.sub binary offset cmdsize - sizeof_load_command) in *)
     let bytes = binary in
-    let t = 
-      match cmd with
-      | LC_SEGMENT_64 ->
-	 LC_SEGMENT_64 (get_segment64 bytes offset)
-      | LC_LOAD_DYLINKER ->
-	 LC_LOAD_DYLINKER (get_dylinker bytes offset)
-      | LC_SYMTAB -> 
-        LC_SYMTAB (get_symtable bytes offset)
-      | LC_DYSYMTAB ->
-        LC_DYSYMTAB (get_dysymtable bytes offset)
-      | LC_DYLD_INFO -> 
-        LC_DYLD_INFO (get_dyld_info bytes offset)
-      | LC_DYLD_INFO_ONLY ->
-        LC_DYLD_INFO (get_dyld_info bytes offset)
-      | LC_ID_DYLIB ->
-        LC_ID_DYLIB (get_dylib_command bytes offset)
-      | LC_LOAD_DYLIB | LC_REEXPORT_DYLIB | LC_LOAD_UPWARD_DYLIB | LC_LAZY_LOAD_DYLIB | LC_LOAD_WEAK_DYLIB -> 
-        LC_LOAD_DYLIB (get_dylib_command bytes offset)
-      | LC_VERSION_MIN_IPHONEOS ->
-        LC_VERSION_MIN_IPHONEOS (get_version bytes offset)
-      | LC_VERSION_MIN_MACOSX ->
-        LC_VERSION_MIN_MACOSX (get_version bytes offset)
-      | LC_MAIN ->
-        LC_MAIN (get_main bytes offset)
-      | _ ->
-        LC_UNIMPLEMENTED {cmd=cmdi; cmdsize}
-    in
+    let t = get_t cmd bytes offset in
     let load_command = {cmd; cmdsize; t} in
-    (*     let load_command = (cmd, cmdsize, lc_t) in *)
     get_load_commands_it binary (offset + cmdsize) (ncmds - 1) (load_command::acc)
 
 let get_load_commands binary offset ncmds sizeofcmds : t =
