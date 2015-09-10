771d793bf9bf4d35394f075c150abf1dc86b6b3a
m4b <m4b.github.io@gmail.com>
2015-09-09 21:01:32 -0600

    fix borken pe byte coverage commit;

--- lib/pe/PEByteCoverage.ml
+++ lib/pe/PEByteCoverage.ml
@@ -3,28 +3,28 @@ open PEHeader
 
 let debug = false
 
-let compute_import_data_coverage data_directory data =
+let compute_import_data_coverage data_directory export_data sections data =
   data
 
-let compute_export_data_coverage data_directory sections data =
+let compute_export_data_coverage data_directory (export_data:PEExport.export_data) sections data =
   assert (data_directory.export_table <> 0);
   let size = data_directory.size_of_export_table in
-  try 
+  try
     let r1 = 
       PEUtils.find_offset data_directory.export_table sections
     in
     let r2 = r1 + size in
-    (create_data
-       ~tag:tag
-       ~r1:r1
-       ~r2:r2
-       ~extra:extra
-       ~understood:true
-    ) data
+    ByteCoverage.add
+      (create_data
+         ~tag:Meta
+         ~r1:r1
+         ~r2:r2
+         ~extra:"Export Table"
+         ~understood:true
+      ) data
   with Not_found -> data
 
-
-let compute_data_directory_coverage optional_header export_data import_data sections data =
+let compute_data_directory_coverage optional_header (export_data:PEExport.export_data option) import_data sections data =
   match optional_header with
   | None ->
     data
@@ -32,13 +32,13 @@ let compute_data_directory_coverage optional_header export_data import_data sect
     let dd = header.data_directories in
     let data =
       match export_data with
-      | Some data ->
-        compute_export_data_coverage dd sections data
+      | Some d ->
+        compute_export_data_coverage dd d sections data
       | _ -> data
     in
     match import_data with
-    | Some data ->
-      compute_import_data_coverage dd data
+    | Some d ->
+      compute_import_data_coverage dd d sections data
     | _ -> data
 
 module SectionMap = Map.Make(String)
@@ -65,29 +65,21 @@ let compute_section_table_coverage sections data =
       let r1 = section.pointer_to_raw_data in
       let r2 = r1 + section.size_of_raw_data in
       let extra = "Section Table // " ^ section.name in
-      let elt =       
-        if (SectionMap.mem section.name known_sections) then
-          let tag = SectionMap.find section.name known_sections in
+      if (SectionMap.mem section.name known_sections) then
+        let tag = SectionMap.find section.name known_sections in
+        ByteCoverage.add
           (create_data
              ~tag:tag
              ~r1:r1
              ~r2:r2
              ~extra:extra
              ~understood:true
-          )
-        else
-          (create_data
-             ~tag:Unknown
-             ~r1:r1
-             ~r2:r2
-             ~extra:extra
-             ~understood:false
-          )
-      in
-      ByteCoverage.add elt acc
+          ) data
+      else
+        data
     ) data sections
 
-let compute_byte_coverage header size export_data import_data sections binary :ByteCoverage.t =
+let compute_byte_coverage header size (export_data:PEExport.export_data option) (import_data:PEImport.import_data option) sections binary :ByteCoverage.t =
   let dos_end = header.dos_header.pe_pointer in
   let coff_end = dos_end + PEHeader.sizeof_coff_header in
   let optional_end = coff_end + header.coff_header.size_of_optional_header in
